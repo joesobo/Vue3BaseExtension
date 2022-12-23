eeb5858c07abd7ce4140ee9b5f330967808ca1da
joesobo <soboleskijoe@gmail.com>
2022-12-23 13:15:14 -0800

    working

--- README.md
+++ README.md
@@ -1,13 +1,24 @@
-# Vue 3 Webview Base Extension README
+# Calico Colors — Webview View API Sample
 
-This is the README for your extension "vue3baseextension". After writing up a brief description, we recommend including the following sections.
+Demonstrates VS Code's proposed [webview view API](https://github.com/microsoft/vscode/issues/46585). This includes:
 
-https://code.visualstudio.com/api/extension-guides/webview
+- Contributing a webview based view to the explorer.
+- Posting messages from an extension to a webview view
+- Posting message from a webview to an extension  
+- Persisting state in the view.
+- Contributing commands to the view title.
 
-## Features
+## VS Code API
 
-- [ ] Vue 3
-- [ ] Tailwind
-- [ ] Vite
-- [ ] Vue Linting
-- [ ] Vue Typechecking
+### `vscode` module
+
+- [`window.registerWebviewViewProvider`](https://code.visualstudio.com/api/references/vscode-api#window.registerWebviewViewProvider)
+
+## Running the example
+
+- Open this example in VS Code 1.49+
+- `npm install`
+- `npm run watch` or `npm run compile`
+- `F5` to start debugging
+
+In the explorer, expand the `Calico Colors` view.
--- src/BaseViewProvider.ts
+++ src/BaseViewProvider.ts
@@ -1,23 +1,28 @@
-import * as vscode from "vscode";
+
+import * as vscode from "vscode"
 
 export class BaseViewProvider implements vscode.WebviewViewProvider {
-	public static readonly viewType = "vue3BaseExtension";
+	public static readonly viewType = "base-view-sidebar";
 
 	private _view?: vscode.WebviewView;
 
-	constructor(private readonly _extensionUri: vscode.Uri) {}
+	constructor(
+		private readonly _extensionUri: vscode.Uri,
+	) { }
 
 	public resolveWebviewView(
 		webviewView: vscode.WebviewView,
 		context: vscode.WebviewViewResolveContext,
-		_token: vscode.CancellationToken
+		_token: vscode.CancellationToken,
 	) {
 		this._view = webviewView;
 
 		webviewView.webview.options = {
 			// Allow scripts in the webview
 			enableScripts: true,
-			localResourceRoots: [this._extensionUri],
+			localResourceRoots: [
+				this._extensionUri
+			]
 		};
 
 		webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
@@ -25,30 +30,50 @@ export class BaseViewProvider implements vscode.WebviewViewProvider {
 
 	private _getHtmlForWebview(webview: vscode.Webview) {
 		// Get the local path to main script run in the webview, then convert it to a uri we can use in the webview.
-		// const scriptUri = webview.asWebviewUri(
-		// 	vscode.Uri.joinPath(this._extensionUri, "media", "main.js")
-		// );
+		const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, "media", "main.js"));
 
-		const styleResetUri = webview.asWebviewUri(
-			vscode.Uri.joinPath(this._extensionUri, "assets/css", "reset.css")
-		);
-		const styleVSCodeUri = webview.asWebviewUri(
-			vscode.Uri.joinPath(this._extensionUri, "assets/css", "vscode.css")
-		);
+		// Do the same for the stylesheet.
+		const styleResetUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, "assets/css", "reset.css"));
+		const styleVSCodeUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, "assets/css", "vscode.css"));
+		const styleMainUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, "assets/css", "main.css"));
+
+		// Use a nonce to only allow a specific script to be run.
+		const nonce = getNonce();
 
 		return `<!DOCTYPE html>
 			<html lang="en">
 			<head>
 				<meta charset="UTF-8">
+
+				<!--
+					Use a content security policy to only allow loading styles from our extension directory,
+					and only allow scripts that have a specific nonce.
+					(See the 'webview-sample' extension sample for img-src content security policy examples)
+				-->
+				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
+
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 				<link href="${styleResetUri}" rel="stylesheet">
 				<link href="${styleVSCodeUri}" rel="stylesheet">
+				<link href="${styleMainUri}" rel="stylesheet">
+
+				<title>Base View Extension</title>
 			</head>
 			<body>
-				<h1>Hello World from Base View Extension!</h1>
+				<h1>Hello World!</h1>
+
+				<script nonce="${nonce}" src="${scriptUri}"></script>
 			</body>
-			</html>`
+			</html>`;
 	}
 }
 
-
-// <script nonce="${nonce}" src="${scriptUri}"></script>
+function getNonce() {
+	let text = "";
+	const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	for (let i = 0; i < 32; i++) {
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
+	}
+	return text;
+}
